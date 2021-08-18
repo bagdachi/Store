86597f446c2e979b27f0fdc2c430f8f5dae591eb
Chirag <chiragbagda@gmail.com>
2021-08-18 21:47:11 +0530

    Initial Commit
    User - entity, interface, junit done

---
+++ code.store/src/main/java/com/example/demo/User.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column( nullable = false , length = 40 )
+	private String name;
+	
+	@Column( nullable = false , unique = true, length = 30 )
+	private String mail;
+	
+	@Column( nullable = false , length = 10 )
+	private String number;
+	
+	@Column( nullable = false , length = 20 )
+	private String password;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
---
+++ code.store/src/test/java/com/example/demo/UserRepositoryTests.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateTestUser() {
+		User user =new User();
+		user.setMail("neel@sap.com");
+		user.setName("Neel patel");
+		user.setNumber("9239312234");
+		user.setPassword("neel@123");
+		
+		User saveUser= repo.save(user);
+		
+		User existUser = entityManager.find(User.class, saveUser.getId());
+		
+		assertThat(existUser.getMail()).isEqualTo(user.getMail());
+				
+		
+		
+	}
+}
